4694487d9a20de2afaf0269a12d4126e33353578
Chuanqing.Wu <via313@gmail.com>
2016-03-15 16:56:57 +0800

    URLConnection

---
+++ app/src/main/java/com/brand/ushopping/utils/URLConnectionUtil.java
@@ -0,0 +1,18 @@
+package com.brand.ushopping.utils;
+
+/**
+ * Created by Administrator on 2016/3/15.
+ */
+public class URLConnectionUtil
+{
+    public static String post(String url, String params)
+    {
+        String result = "";
+
+
+
+        return result;
+    }
+
+
+}
